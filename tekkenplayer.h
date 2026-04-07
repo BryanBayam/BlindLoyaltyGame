38f59b86c2f61890100c8df390dc80c02f6da1a2
unknown <bryanmatalie263@gmail.com>
2026-04-07 20:42:04 +0800

    update tekken

---
+++ tekkenplayer.h
@@ -0,0 +1,51 @@
+#ifndef TEKKENPLAYER_H
+#define TEKKENPLAYER_H
+
+#include "raylib.h"
+#include <stdbool.h>
+
+// Define all character states
+typedef enum {
+    STATE_IDLE = 0,
+    STATE_WALK,
+    STATE_RUN,
+    STATE_JUMP,
+    STATE_SHIELD,
+    STATE_ATTACK_1,
+    STATE_ATTACK_2,
+    STATE_ATTACK_3,
+    STATE_HURT,
+    STATE_DEAD,
+    NUM_STATES
+} CharState;
+
+// Character structure
+typedef struct {
+    Vector2 position;
+    Vector2 velocity;
+    CharState state;
+    bool facingRight;
+    bool isGrounded;  
+    int health;
+    
+    // Animation properties
+    Texture2D textures[NUM_STATES];
+    int frameCounts[NUM_STATES];
+    int currentFrame;
+    float frameTimer;
+    float frameDuration;
+    
+    // Hitbox
+    Rectangle hitBox;
+
+    // AI properties
+    float aiTimer;
+} Character;
+
+// Function prototypes
+void InitCharacter(Character* c, Vector2 startPos, bool isPlayer);
+void UpdateCharacter(Character* c, Character* opponent, float dt, bool isPlayer);
+void DrawCharacter(Character* c);
+void DrawGameUI(int playerHealth, int enemyHealth, int timeRemaining, int screenWidth);
+
+#endif // TEKKENPLAYER_H
